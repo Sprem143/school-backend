a12be88a49325bc603c7a06f760d48ae1175b5a7
Prem Kumar <prem68265@gmail.com>
2024-05-08 22:53:03 +0530

    port fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose= require('mongoose');
-const PORT = process.env.PORT || 8050;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -20,7 +19,7 @@ app.use(bodyParser.json());
 
 app.use('/',require('./router.js'));
 
-app.listen(PORT, function(err){
+app.listen(process.env.PORT, function(err){
     if(err){
         console.log(err)
     }else{
