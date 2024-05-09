ae89d35762382dfec1428c147706059c8de4668d
Prem Kumar <prem68265@gmail.com>
2024-05-09 14:14:23 +0530

    cors fix1

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express= require('express')
 const app = express();
 const cors = require('cors');
+app.use(cors());
 const bodyParser = require('body-parser');
 const mongoose= require('mongoose');
 require('dotenv').config();
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 const connectDb = require('./config/mongoose')
 connectDb();
 
-
-
 // app.use(express.json());
 app.use(bodyParser.json());
-
-
 
 app.use('/',require('./router.js'));
 
